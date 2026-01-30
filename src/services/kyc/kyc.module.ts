ac79dc441597b733a14a920e7586942588c00fd7
Ifeoluwa Adegbite <ifeoluwaadegbite5@gmail.com>
2026-01-30 05:20:08 +0100

    feat(kyc): add EncryptionModule to KycModule imports

--- src/services/kyc/kyc.module.ts
+++ src/services/kyc/kyc.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { KycService } from './kyc.service';
 import { KycController } from './kyc.controller';
+import { EncryptionModule } from '../encryption/encryption.module';
 import { MediaUploadService } from '../media/media-upload.service';
 import { PrismaService } from '../database/prisma.service';
 
 @Module({
+  imports: [
+    EncryptionModule,
+  ],
   controllers: [KycController],
-  providers: [KycService, PrismaService , MediaUploadService],
+  providers: [
+    KycService,
+    PrismaService,
+    MediaUploadService,
+  ],
 })
 export class KycModule {}
